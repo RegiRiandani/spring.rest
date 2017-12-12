15186e1e874915cf83a9bccaecd7d2a323cadedf
Regi Riandani <Regi Riandani@RegiRiandani-PC>
2017-12-12 16:06:48 +0700

    commit -12122017

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,11 +1,22 @@
 package hello;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityManagerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +26,37 @@ public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
-
+    
+    @Autowired
+    private Greeting x;
+    
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+    public Greeting greeting() {
+        return x;
     }
+    
+   @Autowired
+   private EntityManagerFactory em;
+   @CrossOrigin(origins= {"*"})
+    @RequestMapping("/actors")
+    public List<Actor> allActors(){
+    	return em  .createEntityManager().createQuery("from Actor").getResultList();
+    }
+   @CrossOrigin(origins= {"*"})
+    @RequestMapping("/films")
+    public List<Film> allFilms(){
+    	return em  .createEntityManager().createQuery("from Film").getResultList();
+    }
+    
+    @RequestMapping("/filmactors")
+    public List<FilmActor> allFilmsActor(){
+    	return em  .createEntityManager().createQuery("from FilmActor").getResultList();
+    }
+    //@RequestMapping("/greeting")
+//    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+//        return new Greeting(counter.incrementAndGet(),
+//                            String.format(template, name));
+//    }
     @RequestMapping("/data")
     public List<String> dataNegara(@RequestParam("pre") String prefix){
     	List<String> data = new ArrayList();
@@ -35,5 +71,29 @@ public class GreetingController {
     	return data.stream()
     			.filter(line->line.contains(prefix))//startsWith(prefix))//contains(prefix))// startsWith(prefix))
     			.collect(Collectors.toList());
+    }
+    @RequestMapping("/countries")
+    public String getCountries() throws IOException {
+    	URL url = new URL("http://www.webservicex.net/globalweather.asmx/GetCitiesByCountry?CountryName=German");
+    	InputStream stream= url.openConnection().getInputStream();
+    	URLConnection connection =url.openConnection();
+    	//connection.setDoOutput(true);
+    	//connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+    	//connection.setRequestProperty("Content-Length", "0");
+    	/*bisa juga*/
+    	 InputStreamReader reader = new InputStreamReader(stream);
+    	  BufferedReader buffer = new BufferedReader(reader);
+    	  
+    	 
+    	String line;
+    	StringBuilder builder =  new StringBuilder();
+    	while((line=buffer.readLine())!=null)
+    	{
+    	builder.append(line);	
+    	}
+    	return builder.toString();
+    	//return buffer.lines().collect(Collectors.joining("\n"));
+    	//return new BufferedReader(new InputStreamReader(stream)).lines().collect(Collectors.joining("\n"));
+    	
     }
 }
